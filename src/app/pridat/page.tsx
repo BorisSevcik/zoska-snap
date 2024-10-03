68fa9f7e2b6e8f26de7ca90fb0e6db1abd449d72
BorisSevcik <boris.sevcik1@gmail.com>
2024-10-03 13:12:35 +0200

    KostraUpdate

---
+++ src/app/pridat/page.tsx
@@ -0,0 +1,16 @@
+// src/app/pridat/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "pridanie | ZoškaSnap" };
+
+export default function Add() {
+
+  return (
+    <Container>
+      <Typography> pridanie </Typography>
+    </Container>
+  );
+}
