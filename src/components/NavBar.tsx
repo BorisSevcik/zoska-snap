0f6713a3e8eb6d7774d6adbdc6681b24131f5ad6
BorisSevcik <boris.sevcik1@gmail.com>
2024-11-27 11:59:10 +0100

    prispekvy

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
       >
         <BottomNavigationAction label="Domov" value="/" icon={<HomeIcon />} />
         <BottomNavigationAction label="Profily" value="/profil" icon={<AccountCircleIcon />} />
-        <BottomNavigationAction label="Príspevky" value="/prispevok" icon={<AddCircleIcon />} />
+        <BottomNavigationAction label="Príspevky" value="/prispevky" icon={<AddCircleIcon />} />
         <BottomNavigationAction label="O mne" value="/o-mne" icon={<InfoIcon />} />
       </BottomNavigation>
     </Box>
